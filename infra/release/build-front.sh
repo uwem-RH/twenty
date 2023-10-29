ddcaf81ae56fa164932d30b34ad644b0e048eea0
Charles Bochet <charles@twenty.com>
2023-10-29 23:56:07 +0100

    bump version

--- infra/release/build-front.sh
+++ infra/release/build-front.sh
@@ -3,4 +3,4 @@ docker buildx build \
 --push \
 --no-cache \
 --platform linux/amd64,linux/arm64 \
--f ./infra/build/front/Dockerfile -t twentycrm/twenty-front:0.1.5 -t twentycrm/twenty-front:latest .
+-f ./infra/build/front/Dockerfile -t twentycrm/twenty-front:0.2.0-beta  .
--- infra/release/build-server.sh
+++ infra/release/build-server.sh
@@ -3,4 +3,4 @@ docker buildx build \
 --push \
 --no-cache \
 --platform linux/amd64,linux/arm64 \
--f ./infra/build/server/Dockerfile -t twentycrm/twenty-server:0.1.5 -t twentycrm/twenty-server:latest .
+-f ./infra/build/server/Dockerfile -t twentycrm/twenty-server:0.2.0-beta .
